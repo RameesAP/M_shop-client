f1c9386f1f01b8afdd07d515e93cf7f25e12b344
RameesAP <rameeswork@gmail.com>
2024-02-28 17:01:52 +0530

    redux updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import Navbar from "./components/Nav";
-
 import React, { useState } from "react";
 import User from "./pages/User";
 import AddJob from "./pages/AddJob";
+import SignIn from "./pages/SignIn";
+import { useSelector } from "react-redux";
+import PrivateRoute from "./components/Privaterouter";
 
 function App() {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const { currentUser } = useSelector((state) => state.user);
+
+  // const excludedRoutes = ["/sign-in"];
+  // const HideMenu = excludedRoutes.some((path) =>
+  //   window.location.pathname.startsWith(path)
+  // );
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
   return (
     <BrowserRouter>
-      <Navbar toggleSidebar={toggleSidebar} />
-      <Sidebar isOpen={isSidebarOpen} />
+      {currentUser && <Navbar toggleSidebar={toggleSidebar} />}
+      {currentUser && <Sidebar isOpen={isSidebarOpen} />}
       <Routes>
-        <Route path="/sign-in" element />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/addjob" element={<AddJob />} />
+        <Route
+          path="/sign-in"
+          element={currentUser ? <Navigate to="/" /> : <SignIn />}
+        />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/addjob" element={<AddJob />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -8,13 +8,12 @@ import {
   signInSuccess,
 } from "../redux/user/userSlice";
 
-
 const SignIn = () => {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const BASE_URL = "https://credotbackramees.onrender.com";
+  // const BASE_URL = "https://credotbackramees.onrender.com";
 
   const handleChange = (e) => {
     setFormData({
@@ -28,7 +27,7 @@ const SignIn = () => {
     try {
       dispatch(signInStart());
 
-    //   const res = await fetch(`${BASE_URL}/api/auth/signin`, {
+      // const res = await fetch(`${BASE_URL}/api/auth/signin`, {
       const res = await fetch(`/api/auth/signin`, {
         method: "POST",
         headers: {
@@ -48,8 +47,8 @@ const SignIn = () => {
     }
   };
   return (
-    <div className="flex justify-between items-center max-w-7xl  mx-auto  p-3">
-      <div className="w-full h-[787px] bg-[#F4F4F4]  flex items-center justify-center">
+    <div className="flex justify-between items-center   mx-auto ">
+      <div className="w-full h-screen bg-[#F4F4F4]  flex items-center justify-center">
         <div className=" h-[555px] w-[656px] flex flex-col items-center justify-center">
           <h1 className="font-bold text-xl mb-5">Login to your account</h1>
           <form
@@ -57,38 +56,22 @@ const SignIn = () => {
             className="flex flex-col gap-4 items-center"
           >
             <input
-              className="w-96 h-[60px]"
+              className="w-96 h-[60px] p-2"
               type="email"
               placeholder="Email"
               id="email"
               onChange={handleChange}
             />
             <input
-              className="w-96 h-[60px]"
+              className="w-96 h-[60px] p-2"
               type="password"
               placeholder="Password"
               id="password"
               onChange={handleChange}
             />
-            <div className=" flex items-center justify-center">
-              <div className=" uppercase w-14 h-14 rounded-full flex items-center justify-center bg-white font-semibold mt-5">
-                or
-              </div>
-            </div>
-
-        
-
             <button className="w-[176px] h-[52px] border bg-[#1AA5C3] text-white uppercase font-semibold">
               Login
             </button>
-            <div className="">
-              <div className="flex">
-                <p className="mr-2 font-light">Don't have an account?</p>
-                <Link to={"/sign-up"}>
-                  <span className="text-[#1AA5C3] ">Sign Up</span>
-                </Link>
-              </div>
-            </div>
             {error && <p className="text-red-500 mt-5">{error}</p>}
           </form>
         </div>
