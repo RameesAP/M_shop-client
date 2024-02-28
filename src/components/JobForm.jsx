c83fd40f8421466cf6abe2a04a39948cf38ac704
RameesAP <rameeswork@gmail.com>
2024-02-29 00:42:40 +0530

    update

--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -1,29 +1,79 @@
 import React, { useState } from "react";
 
 const JobForm = () => {
-  const [formData, setFormData] = useState({});
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
+  const [deliveryOption, setDeliveryOption] = useState("");
+  const [formData, setFormData] = useState({
+    username: "",
+    address: "",
+    problem: "",
+    number: "",
+    brand: "",
+    model: "",
+    category: "",
+    condition: "",
+    place: "",
+    ime: "",
+    password: "",
+    remark: "",
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleDeliveryOptionChange = (e) => {
+    setDeliveryOption(e.target.value);
   };
 
-  const handleSubmit = async (e) =>{
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res =await fetch("/api/user/createjob",{
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json",
+      const res = await fetch("/api/user/createjob", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify(formData),
-      })
-    } catch (error) {
-      console.log(error);
-    }
-  }
+        body: JSON.stringify({
+          ...formData,
+          deliveryOption: deliveryOption,
+        }),
+      });
 
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+
+      setFormData({
+        username: "",
+        address: "",
+        problem: "",
+        number: "",
+        brand: "",
+        model: "",
+        category: "",
+        condition: "",
+        place: "",
+        ime: "",
+        password: "",
+        remark: "",
+      });
+      setDeliveryOption("");
+      window.location.reload()
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
 
   return (
     <div>
@@ -116,11 +166,11 @@ const JobForm = () => {
               htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Model Number
+              Model
             </label>
             <input
               type="text"
-              id="modelnumber"
+              id="model"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Model Number"
               required
@@ -156,7 +206,7 @@ const JobForm = () => {
               type="text"
               id="condition"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Model Number"
+              placeholder="Condition"
               required
               onChange={handleChange}
             />
@@ -172,15 +222,113 @@ const JobForm = () => {
               type="text"
               id="place"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Category"
+              placeholder="Place"
+              required
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="relative z-0 w-full mb-5 group">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              IME No
+            </label>
+            <input
+              type="text"
+              id="ime"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="IME No"
+              required
+              onChange={handleChange}
+            />
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <label
+              htmlFor="category"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Delivery
+            </label>
+            <div className="flex gap-10">
+              <div className="flex items-center mb-4">
+                <input
+                  id="direct"
+                  type="radio"
+                  name="deliveryOption"
+                  value="direct"
+                  onChange={handleDeliveryOptionChange}
+                  checked={deliveryOption === "direct"}
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="direct"
+                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Direct
+                </label>
+              </div>
+              <div className="flex items-center mb-4">
+                <input
+                  id="courier"
+                  type="radio"
+                  name="deliveryOption"
+                  value="courier"
+                  onChange={handleDeliveryOptionChange}
+                  checked={deliveryOption === "courier"}
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="courier"
+                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Courier
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="relative z-0 w-full mb-5 group">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="text"
+              id="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Password"
+              required
+              onChange={handleChange}
+            />
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <label
+              htmlFor="category"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Remark
+            </label>
+            <input
+              type="text"
+              id="remark"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Remark"
               required
               onChange={handleChange}
             />
           </div>
         </div>
         <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Submit
+           {loading ? "Loading" : "Submit"}
         </button>
+        
+        {error && <p className="text-red-500 mt-5">{error}</p>}
       </form>
     </div>
   );
